73615b11201fc7224c64e2ca92eeef63a9a61059
ramity <lcb005@uark.edu>
2016-05-31 11:59:27 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+	<head>
+		<title>Ecommerce Home</title>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="https://ramity.com/projects/ecommerce/css/index.css">
+		<!--
+		<meta charset='UTF-8'>
+		<meta name='keywords' content='your, tags'>
+		<meta name='description' content='150 words'>
+		<meta name='subject' content='your website's subject'>
+		<meta name='copyright' content='company name'>
+		<meta name='language' content='ES'>
+		<meta name='robots' content='index,follow'>
+		<meta name='revised' content='Sunday, July 18th, 2010, 5:15 pm'>
+		<meta name='abstract' content=''>
+		<meta name='topic' content=''>
+		<meta name='summary' content=''>
+		<meta name='Classification' content='Business'>
+		<meta name='author' content='name, email@hotmail.com'>
+		<meta name='designer' content=''>
+		<meta name='reply-to' content='email@hotmail.com'>
+		<meta name='owner' content=''>
+		<meta name='url' content='http://www.websiteaddrress.com'>
+		<meta name='identifier-URL' content='http://www.websiteaddress.com'>
+		<meta name='directory' content='submission'>
+		<meta name='pagename' content='jQuery Tools, Tutorials and Resources - O'Reilly Media'>
+		<meta name='category' content=''>
+		<meta name='coverage' content='Worldwide'>
+		<meta name='distribution' content='Global'>
+		<meta name='rating' content='General'>
+		<meta name='revisit-after' content='7 days'>
+		<meta name='subtitle' content='This is my subtitle'>
+		<meta name='target' content='all'>
+		<meta name='HandheldFriendly' content='True'>
+		<meta name='MobileOptimized' content='320'>
+		<meta name='date' content='Sep. 27, 2010'>
+		<meta name='search_date' content='2010-09-27'>
+		<meta name='DC.title' content='Unstoppable Robot Ninja'>
+		<meta name='ResourceLoaderDynamicStyles' content=''>
+		<meta name='medium' content='blog'>
+		<meta name='syndication-source' content='https://mashable.com/2008/12/24/free-brand-monitoring-tools/'>
+		<meta name='original-source' content='https://mashable.com/2008/12/24/free-brand-monitoring-tools/'>
+		<meta name='verify-v1' content='dV1r/ZJJdDEI++fKJ6iDEl6o+TMNtSu0kv18ONeqM0I='>
+		<meta name='y_key' content='1e39c508e0d87750'>
+		<meta name='pageKey' content='guest-home'>
+		<meta itemprop='name' content='jQTouch'>
+		<meta http-equiv='Expires' content='0'>
+		<meta http-equiv='Pragma' content='no-cache'>
+		<meta http-equiv='Cache-Control' content='no-cache'>
+		<meta http-equiv='imagetoolbar' content='no'>
+		<meta http-equiv='x-dns-prefetch-control' content='off'>
+		-->
+	</head>
+	<body>
+		<div id="header">
+			<div id="headerinr">
+				<div id="headerlogo">Ramity Store</div>
+				<div id="headernav">
+					<div class="headernavitem">Home<img class="headernavitemsprite" src="https://ramity.com/projects/ecommerce/img/darrow.png">
+						<div class="headernavitemdrop"></div>
+					</div>
+					<div class="headernavitem">Featured</div>
+					<div class="headernavitem">Blog</div>
+					<div class="headernavitem">Community<img class="headernavitemsprite" src="https://ramity.com/projects/ecommerce/img/darrow.png">
+						<div class="headernavitemdrop"></div>
+					</div>
+					<div class="headernavitem">Process</div>
+					<div class="headernavitem">Contact Us</div>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<div id="sidecontent">
+					<div id="sidenav">
+						<div id="sidenavtitle">
+							Categories
+						</div>
+						<div class="sidenavitem">
+							Computer Hardware
+						</div>
+						<div class="sidenavitem">
+							Computers & Tablets
+						</div>
+						<div class="sidenavitem">
+							Electronics
+						</div>
+						<div class="sidenavitem">
+							Software
+						</div>
+						<div class="sidenavitem">
+							Gaming
+						</div>
+						<div class="sidenavitem">
+							Cell Phones
+						</div>
+						<div class="sidenavitem">
+							Home & Outdoors
+						</div>
+						<div class="sidenavitem">
+							Health & Beauty
+						</div>
+						<div class="sidenavitem">
+							Automotive & Industrial
+						</div>
+						<div class="sidenavitem">
+							Office Products
+						</div>
+						<div class="sidenavitem">
+							Sporting Goods
+						</div>
+						<div class="sidenavitem">
+							Watches & Jewelry
+						</div>
+						<div class="sidenavitem">
+							Apparel & Accessories
+						</div>
+						<div class="sidenavitem">
+							Services
+						</div>
+						<div class="sidenavitem">
+							More
+						</div>
+					</div>
+				</div>
+				<div id="rightcontent">
+					<div id="topnav">
+						<form id="topnavsearch" action="https://ramity.com/projects/ecommerce/" method="GET">
+							<input id="topnavsearchtext" type="text" value="" name="" placeholder="Search...">
+							<input id="topnavsearchsubmit" type="submit" value="Search" name="">
+						</form>
+						<div id="topnavcart"><img src="https://ramity.com/projects/ecommerce/img/scart.png">(0)</div>
+					</div>
+					<div id="gallery">
+						<div id="gallerytitle">
+							<div id="gallerytitlephoto"></div>
+							<div id="gallerytitletext">"We're the company that builds the most immersive PCs." - Lewis Brown, Company CEO</div>
+						</div>
+						<div id="galleryphoto"></div>
+						<div class="galleryproductname">
+							The Juggernaut
+							<div class="galleryproductprice">$955.41</div>
+						</div>
+						<div id="galleryspecs">
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/d8c4e4eed5d7820248be99b5d7dd5dbd.med.256p.jpg">
+								<div class="galleryspecsitemtitle">CPU</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">AMD FX-8350 4.0GHz 8-Core Processor</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoresevenstars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">7 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$158.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/91e1372cfc568bfec8b1d357976e8fe2.med.256p.jpg">
+								<div class="galleryspecsitemtitle">CPU Cooler</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">Corsair H60 54.0 CFM Liquid CPU Cooler</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoreeightstars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">8 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$59.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/bd2326c60febc2d16a8742f041b87fca.256p.jpg">
+								<div class="galleryspecsitemtitle">Motherboard</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">ASRock 970 Performance ATX AM3+ Motherboard</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoreninestars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">9 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$90.98</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/61514edfc0324c1f00e528b25004cc2c.med.256p.jpg">
+								<div class="galleryspecsitemtitle">Memory</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">Corsair Vengeance Pro 8GB (2 x 4GB) DDR3-2400 Memory</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoreninestars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">9 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$92.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/cfae225dd3e43bf766ab1468189a4703.thumb.256p.jpg">
+								<div class="galleryspecsitemtitle">Storage</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">Western Digital Caviar Blue 1TB 3.5" 7200RPM Internal HD</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoresixstars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">6 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$52.49</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/9748bab7e1150d053ac839fcd70d90c2.med.256p.jpg">
+								<div class="galleryspecsitemtitle">Video Card</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">Asus GeForce GTX 970 4GB STRIX Video Card</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoreninestars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">9 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$323.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/c82a46937677af867306640f385674cf.med.256p.jpg">
+								<div class="galleryspecsitemtitle">Case</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">Fractal Design Define R4 w/Window ATX Mid Tower Case</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoretenstars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">10 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$96.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecsitem">
+								<img class="galleryspecsitemimg" src="https://cdn.pcpartpicker.com/static/forever/images/product/571430fabdd343e6caaf262b84fdda34.med.256p.jpg">
+								<div class="galleryspecsitemtitle">PSU</div>
+								<div class="galleryspecsitemarticle">Our review</div>
+								<div class="galleryspecsitemname">EVGA SuperNOVA NEX 650W 80+ Gold Fully-Modular ATX PSU</div>
+								<div class="galleryspecsitemreview">
+									<div class="null">
+										<div class="galleryspecsitemreviewbar" id="reviewscoreeightstars"></div>
+									</div>
+									<div class="null">
+										<div class="galleryspecsitemreviewtext">8 out of 10 stars</div>
+									</div>
+								</div>
+								<div class="galleryspecsitemprice">$78.99</div>
+								<div class="galleryspecsitembuy">
+									Add to cart
+								</div>
+							</div>
+							<div class="galleryspecstotal">
+								<div class="galleryspecstotaltitle">Base Total:</div>
+								<div class="galleryspecstotalvalue">$974.42</div>
+								<div class="galleryspecstotaltitle">Promo Discounts:</div>
+								<div class="galleryspecstotalvalue">-$20.00</div>
+								<div class="galleryspecstotaltitle">Shipping:</div>
+								<div class="galleryspecstotalvalue">$0.99</div>
+								<div class="galleryspecstotaltitle">Grand Total:</div>
+								<div class="galleryspecstotalvalue">$955.41</div>
+								<div class="galleryspecstotalbuy">Add parts to cart</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
